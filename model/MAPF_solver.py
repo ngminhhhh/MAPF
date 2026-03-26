4dcadd14f03188f8fdd34e7c2fd9319c7fd852ef
ngminhhhh <ngcminh0410@gmail.com>
2026-03-26 14:24:33 +0700

    feature aggregator

--- model/MAPF_solver.py
+++ model/MAPF_solver.py
@@ -35,7 +35,7 @@ class FeatureExtraction(nn.Module):
         self.extractor = nn.Sequential(*backbone, nn.Flatten(start_dim=1))
 
     def forward(self, x):
-        return self.extractor(x)
+        return self.extractor(x) # (N, H * W * hidden_dim)
 
 class GATHead(nn.Module):
     def __init__(self, in_dim:int, hidden_dim:int, out_dim:int):
@@ -64,7 +64,18 @@ class GATHead(nn.Module):
             dst:    (E,)
             return: alpha (E,), softmax edges with same dst
         """
+        if scores.numel() == 0: return scores 
 
+        max_per_dst = torch.full((num_nodes, ), -torch.inf, device=scores.device, dtype=scores.dtype)
+        max_per_dst.scatter_reduce_(0, dst, scores, reduce="amax", include_self=True)
+        exp_scores = torch.exp(scores - max_per_dst[dst])
+
+        exp_denom = torch.zeros((num_nodes,), device=scores.device, dtype=scores.dtype)
+        exp_denom.index_add_(0, dst, exp_scores)
+
+        alpha = exp_scores / (exp_denom[dst] + 1e-12)
+
+        return alpha # (E, )
 
     def forward(self, x, edges):
         '''
@@ -105,27 +116,69 @@ class GATHead(nn.Module):
 
         return out
 
-
 class FeatureAggregator(nn.Module):
     def __init__(self, in_dim:int, hidden_dim:int, n_heads:int):
         super().__init__()
         self.heads = nn.ModuleList([
-            GATHead(in_dim, hidden_dim)
+            GATHead(in_dim, hidden_dim, hidden_dim)
             for _ in range(n_heads)
         ])
 
     def forward(self, x, edges):
         outs = [head(x, edges) for head in self.heads]
-        return torch.cat(outs, dim=-1) # (N, hidden_dim * n_heads)
+        return torch.cat(outs, dim=-1) # (N, out_dim * n_heads)
 
-class Solver(nn.Module):
-    def __init__(self, obs_channels: int, 
-                 cnn_hidden_dim: int, cnn_n_blocks:int, kernel_size: int):
+class PolicyHead(nn.Module):
+    def __init__(self, in_dim, hidden_dim, n_mlp_layers):
         super().__init__()
 
-        self.feature_extractor = FeatureExtraction(obs_channels, cnn_hidden_dim, cnn_n_blocks, kernel_size)
+        backbone = []
+        for _ in range(n_mlp_layers):
+            backbone.append(nn.Linear(in_features=in_dim, out_features=hidden_dim, bias=False))
+            backbone.append(nn.ReLU())
+            in_dim=hidden_dim
+        
+        self.mlp = nn.Sequential(*backbone)
+        self.actor_head = nn.Linear(in_features=hidden_dim, out_features=5)
+        self.critic_head = nn.Linear(in_features=hidden_dim, out_features=1)
 
     def forward(self, x):
-        feats = self.feature_extractor(x)
+        x = self.mlp(x)
+        logits = self.actor_head(x)
+        value = self.critic_head(x)
 
-        return feats
+        return logits, value
+
+class Solver(nn.Module):
+    def __init__(self, obs_width:int, obs_height:int, obs_channels: int, 
+                 cnn_hidden_dim: int, n_cnn_blocks:int, kernel_size: int,
+                 gat_hidden_dim:int, n_gat_heads:int,
+                 mlp_hidden_dim:int, n_mlp_layers:int
+                 ):
+        super().__init__()
+
+        self.feature_extractor = FeatureExtraction(obs_channels, cnn_hidden_dim, n_cnn_blocks, kernel_size)
+        cnn_out_dim = obs_width * obs_height * cnn_hidden_dim
+
+        self.feature_aggregator = FeatureAggregator(in_dim=cnn_out_dim, hidden_dim=gat_hidden_dim, n_heads=n_gat_heads)
+        self.head = PolicyHead(in_dim=gat_hidden_dim*n_gat_heads, hidden_dim=mlp_hidden_dim, n_mlp_layers=n_mlp_layers)
+
+    def forward(self, x, edges):
+        feats = self.feature_extractor(x)
+        feats = self.feature_aggregator(feats, edges)
+        logits, value = self.head(feats)
+
+        return logits, value
+    
+    @torch.no_grad()
+    def act(self, obs, edges, deterministic=False):
+        logits, values = self.forward(obs, edges)
+        dist = Categorical(logits=logits)
+
+        if deterministic:
+            actions = torch.argmax(logits, dim=-1)
+        else:
+            actions = dist.sample()
+
+        return actions
+        
